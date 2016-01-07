fe7f57dade8a011c26f8f031bb3ef1e8bdfd2546
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 17:58:56 -0800

    incomplete commit

--- calculator.py
+++ calculator.py
@@ -14,36 +14,44 @@ def calculator():
     while True:
         data = raw_input()
         tokens = data.split(" ")
-        if tokens[0] == "q":
-            break
-        elif tokens[0] == "+":
-            addition = add(int(tokens[1]), int(tokens[2]))
-            print addition
-        elif tokens[0] == "-":
-            subtraction = subtract(int(tokens[1]), int(tokens[2]))
-            print subtraction
-        elif tokens[0] == "*":
-            multiplication = multiply(int(tokens[1]), int(tokens[2]))
-            print multiplication
-        elif tokens[0] == "/":
-            division = divide(int(tokens[1]), int(tokens[2]))
-            print division
-        elif tokens[0] == "square":
-            num_square = square(int(tokens[1]))
-            print num_square
-        elif tokens[0] == "cube":
-            num_cube = cube(int(tokens[1]))
-            print num_cube
-        elif tokens[0] == "pow":
-            num_power = power(int(tokens[1]), int(tokens[2]))
-            print num_power
-        elif tokens[0] == "mod":
-            num_mod = mod(int(tokens[1]), int(tokens[2]))
-            print num_mod
-            
 
+        x = len(tokens)
+        if x > 1:
+            i = x - 1
+            while i > 0:      # is there a better way?
+                tokens[i] = int(tokens[i])
+                i = i - 1
 
-          
+            if tokens[0] == "+":
+                addition = add(tokens[1], tokens[2])
+                print addition
+            elif tokens[0] == "-":
+                subtraction = subtract(tokens[1], tokens[2])
+                print subtraction
+            elif tokens[0] == "*":
+                multiplication = multiply(tokens[1], tokens[2])
+                print multiplication
+            elif tokens[0] == "/":
+                division = divide(tokens[1], tokens[2])
+                print division
+            elif tokens[0] == "square":
+                num_square = square(tokens[1])
+                print num_square
+            elif tokens[0] == "cube":
+                num_cube = cube(tokens[1])
+                print num_cube
+            elif tokens[0] == "pow":
+                num_power = power(tokens[1], tokens[2])
+                print num_power
+            elif tokens[0] == "mod":
+                num_mod = mod(tokens[1], tokens[2])
+                print num_mod
+        else:
+            if tokens[0] == "q":
+                break
+            else:
+                print "This is not a valid entry"
+
 
 calculator()
 
