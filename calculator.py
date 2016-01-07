bbc6490f2b065989a27bc1c88538b3faaaa33397
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 16:09:58 -0800

    add working

--- calculator.py
+++ calculator.py
@@ -9,11 +9,19 @@ from arithmetic import *
 
 
 # Your code goes here
-def calculator(data):
 
+def calculator():
     while True:
+        data = raw_input()
         tokens = data.split(" ")
         if tokens[0] == "q":
             break
         elif tokens[0] == "+":
-            
+            addition = add(int(tokens[1]), int(tokens[2]))
+            print addition
+
+
+          
+
+calculator()
+
